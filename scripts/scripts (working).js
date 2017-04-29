d41925b3f9e83ac592d76b6e3c8c9e126689c0ae
Daniel <daniel.999@gmx.de>
2017-04-29 15:24:43 +0200

    Grid now changes normally and added eraser

--- scripts/scripts (working).js
+++ scripts/scripts (working).js
@@ -1,23 +1,12 @@
 var gridSize = 16
 var gridColor = "black";
 var dimension = 1;
-var toggleH = false;
 
 $(document).ready(function(){
 	createGrid(gridSize);
-	$(document).on("mousedown",".block", function(){
-		$(document).on("mousemove",".block", function(){
-			$(this).css("background-color",gridColor);
-		});
+	$(document).on("mouseenter",".block", function(){
+		$(this).css("background-color",gridColor);
 	});
-	$(document).on("mouseup",".block",function(){
-		$(document).unbind("mousemove");
-	});
-	$(".toggleEraser").click(function(){
-		gridColor = "white";
-		$(".toggleEraser").css("border","solid 5px");
-		$(".color").css("opacity","1");
-	})
 	$(".reset").click(function(){
 		var size = 30
 		newGridSize = prompt("How big should your new grid be?");
@@ -28,17 +17,12 @@ $(document).ready(function(){
 			dimension = 1;
 		} else if (newGridSize === "48") {
 			dimension = 3;
-		} else if (newGridSize === "64") {
-			dimension = 4;
-		} else {
-			alert("Only 16/32/48/64 available");
-		}
+		};
 		var newSize = size/dimension
 		$(".block").css({"width":newSize+"px","height":newSize+"px"})
 	});
 	$(".color").click(function(){
 		gridColor = $(this).css("background-color");
-		$(".toggleEraser").css("border","0");
 		$(".color").css("opacity","1");
 		$(this).css("opacity","0.25");
 	});
